89915159d20287efc5bedc3210173655e7e05475
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-10-10 17:16:33 -0300

    Create About_page.jsx

---
+++ src/pages/About_page.jsx
@@ -0,0 +1,13 @@
+import About from "../components/about_page_about_section/About";
+import DevTimeline from "../components/timeline/DevTimeline.jsx";
+import Skills from "../components/skills_about_page/skills";
+
+const About_page = () => {
+  // in this video i will continue to create About page
+  return <>
+    <About />
+    <Skills/>
+    <DevTimeline/>
+  </>;
+};
+export default About_page;
