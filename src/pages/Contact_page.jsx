4d0aabcd7aaba218503ffb46fe13991590ea9ed2
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-10-19 02:40:48 -0300

    Update Contact_page.jsx

--- src/pages/Contact_page.jsx
+++ src/pages/Contact_page.jsx
@@ -1,7 +1,9 @@
 import Contact from "../components/contact_contact_page/Contact";
+import Navbar from "../components/navbar/Navbar";
 
 const Contact_page = () => {
   return <>
+    <Navbar/>
     <Contact />
   </>;
 };
