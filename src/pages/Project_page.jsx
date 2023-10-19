2d0315f83efaecb1f036635f6a58d7ef6dba923e
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-10-19 02:40:34 -0300

    Update Project_page.jsx

--- src/pages/Project_page.jsx
+++ src/pages/Project_page.jsx
@@ -3,12 +3,12 @@
 
 import Complete_apps from "../components/complete-apps/Complete_apps";
 import Small_projects from "../components/small_projects/Small_projects";
+import Navbar from "../components/navbar/Navbar";
 
 const Project_page = () => {
-
-
   return (
     <>
+      <Navbar/>
       <Complete_apps />
       <Small_projects />
     </>
