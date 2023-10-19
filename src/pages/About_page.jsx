f6161b6d19de3670853c7920a143db2df31f7336
Tomas Riera <53879865+tomas26j@users.noreply.github.com>
2023-10-19 02:40:41 -0300

    Update About_page.jsx

--- src/pages/About_page.jsx
+++ src/pages/About_page.jsx
@@ -1,10 +1,12 @@
 import About from "../components/about_page_about_section/About";
 import DevTimeline from "../components/timeline/DevTimeline.jsx";
 import Skills from "../components/skills_about_page/skills";
+import Navbar from "../components/navbar/Navbar";
 
 const About_page = () => {
   // in this video i will continue to create About page
   return <>
+    <Navbar/>
     <About />
     <Skills/>
     <DevTimeline/>
